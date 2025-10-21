d93febe809aad83ce9e223712eb80eee96ef0df8
Justine Vilocura <justine.vilocura@cit.edu>
2025-10-21 17:43:54 +0800

    first commit

---
+++ src/main/java/com/appdev/jeb/vilocurag6/Vilocurag6Application.java
@@ -0,0 +1,13 @@
+package com.appdev.jeb.vilocurag6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vilocurag6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vilocurag6Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/appdev/jeb/vilocurag6/Vilocurag6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appdev.jeb.vilocurag6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vilocurag6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
